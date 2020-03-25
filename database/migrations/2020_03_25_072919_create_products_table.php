00c6ebabbe5b33fa81080b82ab5434e0de22438f
DennisNgugi <denongugi73@gmail.com>
2020-03-25 11:16:33 +0300

    Spattie package and Authentication added

--- database/migrations/2020_03_25_072919_create_products_table.php
+++ database/migrations/2020_03_25_072919_create_products_table.php
@@ -14,8 +14,10 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+          $table->bigIncrements('id');
+       $table->string('name');
+       $table->text('detail');
+       $table->timestamps();
         });
     }
 
